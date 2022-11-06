fe4597bbed514e6e5c7154be632c216f77fced5d
Maryna Skoryk <skoryk.maryna@gmail.com>
2022-11-06 14:33:18 +0100

    1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let activeImageSrc = null;
+let instance = null;
+const galleryRef = document.querySelector('.gallery');
+const markupGallery = createGalleryMarkUp(galleryItems);
+
+function createGalleryMarkUp(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item "><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
+    )
+    .join('');
+}
+
+galleryRef.insertAdjacentHTML('beforeend', markupGallery);
+galleryRef.addEventListener('click', onClickImageGallery);
+
+function onClickImageGallery(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  activeImageSrc = getImageSrc(event);
+  openModal(activeImageSrc);
+}
+function getImageSrc(event) {
+  return event.target.dataset.source;
+}
+
+function openModal() {
+  instance = basicLightbox.create(
+    `
+		<img width="1280" height="852" src="${activeImageSrc}">`
+  );
+  instance.show(() => {
+    window.addEventListener('keydown', closeModalbyEscape);
+  });
+}
+function closeModal() {
+  instance.close(() => {
+    window.removeEventListener('keydown', closeModalbyEscape);
+  });
+}
+
+function closeModalbyEscape(event) {
+  const isEscape = event.code === 'Escape';
+  if (isEscape) {
+    closeModal();
+  }
+}
+console.log(galleryRef);
