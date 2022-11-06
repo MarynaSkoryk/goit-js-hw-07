b34dfaad8dd0dbf8867dc74b2172975e06589389
Maryna Skoryk <skoryk.maryna@gmail.com>
2022-11-06 14:55:19 +0100

    2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const markupGallery = createMarkupGallery(galleryItems);
+
+function createMarkupGallery(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`
+    )
+    .join('');
+}
+
+galleryRef.insertAdjacentHTML('beforeend', markupGallery);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionData: 'alt',
+});
+console.log(gallery);
